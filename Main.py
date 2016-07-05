1b61d83cdde9b1d1280d161748fd225e5b439574
Carre1 <Carre1@fadedsky.cc>
2016-07-06 00:47:25 +0800

    对配置文件的更新（未完成） 加入了以下新的东西： 统计器的开关
    效率极其低

--- Main.py
+++ Main.py
@@ -3,26 +3,30 @@ import linecache
 import time
 import ConfigParser
 import string, os, sys
+import re
 
-#导入配置文件
-latest_path = 'logs/latest.log'
+# latest_path = 'logs/latest.log'
+cf = ConfigParser.ConfigParser()     #实例化
+cf.read('first_instruction.conf')    #读取配置文件
 
-global last_linage    #默认过去行数
-last_linage = 0
+global last_linage
+last_linage = 0    #默认过去行数
+check_wait = 0
 
-def check_status(info):
-    print "done"
+def check_info(info):    #统计消息
+    if str(cf.get("basis","statistics ")) == "True":    #判断功能是否开启
+        pass
 
 while True:
     time.sleep(1)
-    file_object = open(latest_path)
+    file_object = open(str(cf.get("server","server-side_latest_path")))
 
     # print "Done."
-    linage = len(open(latest_path,'rU').readlines())    #获取文件行数
+    linage = len(open(str(cf.get("server","server-side_latest_path")),'rU').readlines())    #获取文件行数
     # print linage
 
     while linage != last_linage:   #如果新旧不吻合
         last_linage = last_linage+1    #下一行号
         linecache.clearcache()   #清理linecache缓存
-        the_line=linecache.getline(latest_path,last_linage)   #扫描下一行内容
-        print "[Server Info "+last_linage+"]",the_line
+        the_line=linecache.getline(cf.get("server","server-side_latest_path"),last_linage)   #扫描下一行内容
+        print "[Server Info "+str(last_linage)+"]",the_line
