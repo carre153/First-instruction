0de1b1827cfa6a45303c8abd5b02385754e3c0e0
Carre1 <Carre1@fadedsky.cc>
2016-07-07 17:51:28 +0800

    加入了日志 加入了开发者选项 没了。。

--- Main.py
+++ Main.py
@@ -1,46 +1,80 @@
 #-*- coding:utf-8 -*-
+
 import linecache
 import time
+import timeit
 import ConfigParser
 import string, os, sys
 import re
+import logging
 
-# latest_path = 'logs/latest.log'
-cf = ConfigParser.ConfigParser()     #实例化
-cf.read('first_instruction.conf')    #读取配置文件
+cf = ConfigParser.ConfigParser()     #载入配置文件
+cf.read('first_instruction.conf')
+ISODATEFORMAT = '%Y-%m-%d_%H-%M' #当前日期
+
+if cf.get('developer','simple_log') == 'True':    #判断需要日志类型
+    format='[%(asctime)s] [%(levelname)s] %(message)s'
+else:
+    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s'
+
+logging.basicConfig(level=logging.DEBUG,    #载入日志
+                format=format,
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename='fi_bin/logs/'+ str(time.strftime( ISODATEFORMAT, time.localtime()) + '.log'),
+                filemode='w')
+
+#将INFO级别或更高的日志信息打印到标准错误，并将其添加到当前的日志处理对象
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('[%(asctime)s] [First instruction/%(levelname)s]: %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
 
 global last_linage
 last_linage = 0    #默认过去行数
 check_wait = 0
 
+logging.info('The First_instruction are start.')
+
+#### 加载结束 ####
+
 def statistical(info):    #统计消息
     if str(cf.get("basis","statistics")) == "True":    #判断功能是否开启
         def s_startserver():    #开启服务器记录
-            records_file = open('fi_bin/statistical_records/start_server.txt','r')
-            read_records_file = records_file.read()    #读取统计记录文件
-            records_file.close()
-            output_records = int(read_records_file)+1
-            output = open('fi_bin/statistical_records/start_server.txt', 'w')    #写入
-            output.write(str(output_records))
-            output.close()
+            try:
+                records_file = open('fi_bin/statistical_records/start_server.txt','r')
+                read_records_file = records_file.read()    #读取统计记录文件
+                records_file.close()
+                output_records = int(read_records_file)+1
+                output = open('fi_bin/statistical_records/start_server.txt', 'w')    #写入
+                output.write(str(output_records))
+                output.close()
+            except:
+                logging.error('Log message failed.Please confirm that fi_bin/statistical_records/start_server.txt is a number')
 
         def s_closeserver():    #关闭服务器记录
-            records_file = open('fi_bin/statistical_records/close_server.txt','r')
-            read_records_file = records_file.read()    #读取统计记录文件
-            records_file.close()
-            output_records = int(read_records_file)+1
-            output = open('fi_bin/statistical_records/close_server.txt', 'w')    #写入
-            output.write(str(output_records))
-            output.close()
+            try:
+                records_file = open('fi_bin/statistical_records/close_server.txt','r')
+                read_records_file = records_file.read()    #读取统计记录文件
+                records_file.close()
+                output_records = int(read_records_file)+1
+                output = open('fi_bin/statistical_records/close_server.txt', 'w')    #写入
+                output.write(str(output_records))
+                output.close()
+            except:
+                logging.error('Log message failed.Please confirm that fi_bin/statistical_records/close_server.txt is a number')
 
         def s_player_up():    #玩家上线记录
-            records_file = open('fi_bin/statistical_records/player_up.txt','r')
-            read_records_file = records_file.read()    #读取统计记录文件
-            records_file.close()
-            output_records = int(read_records_file)+1
-            output = open('fi_bin/statistical_records/player_up.txt', 'w')    #写入
-            output.write(str(output_records))
-            output.close()
+            try:
+                records_file = open('fi_bin/statistical_records/player_up.txt','r')
+                read_records_file = records_file.read()    #读取统计记录文件
+                records_file.close()
+                output_records = int(read_records_file)+1
+                output = open('fi_bin/statistical_records/player_up.txt', 'w')    #写入
+                output.write(str(output_records))
+                output.close()
+            except:
+                logging.error('Log message failed.Please confirm that fi_bin/statistical_records/player_up.txt is a number')
 
         def s_server_command():
             try:
@@ -52,16 +86,16 @@ def statistical(info):    #统计消息
                 output.write(str(output_records))
                 output.close()
             except:
-                print '[Fi Error] Not Can Cannot write command.txt '
+                logging.error('Log message failed.Please confirm that fi_bin/statistical_records/command.txt is a number')
 
         d_info = re.search(r"(?<=]: ).+?(?=$)",info,re.M)
 
         try:
             # print "Fi Info : ",str(d_info)
             if d_info.group(0) == 'Timings Reset':
-                print 'Timings Reset'
+                s_startserver()
             elif d_info.group(0) == 'Stopping server':
-                print 'Server stop'
+                s_closeserver()
             else:
                 pass
         except:
